cc7a55a02edf17d7d855a42d9a9e6009fffff71b
huangxin <1103406663@qq.com>
2020-07-08 09:56:29 +0800

    更新

--- jdqd/a04/relation/relation_pt/services/data_gen.py
+++ jdqd/a04/relation/relation_pt/services/data_gen.py
@@ -11,36 +11,6 @@ from feedwork.utils import FileHelper
 from jdqd.a04.relation.relation_pt.algor import r_then, r_parallel, r_further
 
 
-def extract_articles(article_content, relation, neg_num, with_events=True,
-                     neg_capacity=10000):
-    rsts = []
-    rsts_neg = []
-    sentences = relation_util.split_article_to_sentences(article_content)
-    for sentence in sentences:
-        rst = relation_combine.extract_all_rules(sentence, relation.rules,
-                                                 relation.keyword_rules)
-        if rst:
-            left = rst['left']
-            right = rst['right']
-            keyword = rst['tag_indexes']
-            if (not left) or (not right):
-                continue
-            if with_events:
-                left_events = get_events(left)
-                right_events = get_events(right)
-                line = [sentence, str(keyword), left, right, left_events,
-                        right_events]
-            else:
-                line = [sentence, str(keyword), left, right, '', '']
-            line = '\t'.join(line) + '\n'
-            rsts.append(line)
-        else:
-            if neg_num < neg_capacity:
-                rsts_neg.append(sentence + '\n')
-                neg_num += 1
-    return rsts, rsts_neg, neg_num
-
-
 def zh_ratio(text):
     """
     计算中文字符在文本中所占的比例
@@ -86,25 +56,34 @@ def get_articles():
         return articles_zh
 
     articles = get_articles_from_db()
-    articles_zh = filter_articles(articles)
+    articles_zh = filter_db_articles(articles)
     save_content(json.dumps(articles_zh), txt)
     return articles_zh
 
 
-def save_content(content, fn):
-    with open(fn, 'w', encoding='utf-8') as f:
+def save_content(content, fn, mode='w'):
+    with open(fn, mode, encoding='utf-8') as f:
         f.write(content)
 
 
-def readfile(fn):
+def readfile(fn, readlines=False):
     with open(fn, 'r', encoding='utf-8') as f:
-        return f.read()
+        if readlines:
+            return f.readlines()
+        else:
+            return f.read()
 
 
 event_url = 'http://172.168.0.23:38082/event_extract'
 
 
 def get_events(sentence):
+    """
+    获取句中的事件, 每个事件按照主语, 否定词, 谓语, 宾语的顺序以竖线|连接成str, 多个事件的
+    str以汉字丨(音gun)连接, 返回连接的str
+    :param sentence:
+    :return:
+    """
     print('processing request')
     data = {'sentence': sentence}
     ret = requests.post(event_url, data=data)
@@ -118,15 +97,21 @@ def get_events(sentence):
     return svos
 
 
-def filter_articles(articles):
+def filter_article(content):
+    content = remove_white_space(content)
+    content = remove_html_tags(content)
+    return content
+
+
+def filter_db_articles(articles):
+    """"""
     filtered = []
     for atc in articles:
         id_ = atc['article_id']
         content = atc['content']
         if content is None or len(content) < 4:
             continue
-        content = remove_white_space(content)
-        content = remove_html_tags(content)
+        content = filter_article(content)
         # 如果文章内容全为被remove的内容, 则此时长度有可能为0, 计算语言比例时会
         # divide by zero, 故再次长度判断
         if len(content) < 4:
@@ -136,77 +121,153 @@ def filter_articles(articles):
     return filtered
 
 
-def save_ner_data():
-    rst_articles = []
+def extract_article_relation(article_content, relation, neg_num, neg_capacity):
+    """
+    从文章的句子中抽取关系, 根据是否有关系创建关联词模型的正负样本.
+    :param article_content:
+    :param relation:
+    :param neg_num: 用于记录负样本数量
+    :param neg_capacity:
+    :return:
+    """
+    rsts_pos = []
+    rsts_neg = []
+    sentences = relation_util.split_article_to_sentences(article_content)
+    for sentence in sentences:
+        rst = relation_combine.extract_all_rules(sentence, relation.rules,
+                                                 relation.keyword_rules)
+        if rst:
+            left = rst['left']
+            right = rst['right']
+            if (not left) or (not right):
+                continue
+            keyword = json.dumps(rst['tag_indexes'])
+            line = [sentence, keyword, left, right]
+            line = '\t'.join(line) + '\n'
+            rsts_pos.append(line)
+        else:
+            if neg_num < neg_capacity:
+                rsts_neg.append(sentence + '\n')
+                neg_num += 1
+    return rsts_pos, rsts_neg, neg_num
+
+
+def extract_articles_relation(articles_dir, relation, neg_capacity=10000):
+    """
+    遍历本地磁盘上的文章内容生成关键词模型训练数据,
+    :param articles_dir:
+    :param relation:
+    :param neg_capacity:
+    :return:
+    """
+    rst_articles_pos = []
     rst_articles_neg = []
-    # articles_zh = get_articles()
-    relation = r_parallel
-    # 格式: 原句\t{'keyword':[start, end]}\t左句\t右句\t左句事件列表\t右句事件列表
-    # 其中, 左右句事件列表由事件组成, 以汉字丨(音gun)分隔, 每个事件包含主语负词谓语宾语,
-    # 以|分隔
-    articles_dir = r'C:\work1\qingdao\archive\articles\shizheng2'
     articles_fn = os.listdir(articles_dir)
     neg_num = 0
     for fn in tqdm(articles_fn):
         fp = os.path.join(articles_dir, fn)
         with open(fp, 'r', encoding='utf-8') as f:
             lines = f.readlines()
-            if len(lines) < 2:
+        if len(lines) < 2:
+            continue
+        lines = lines[1:]
+        content = ''.join(lines)
+        content = remove_white_space(content)
+        rsts, rsts_neg, neg_num = extract_article_relation(content, relation,
+                                                           neg_num,
+                                                           neg_capacity)
+        rst_articles_pos.extend(rsts)
+        rst_articles_neg.extend(rsts_neg)
+    return rst_articles_pos, rst_articles_neg
+
+
+def save_ner_data(rst_articles_pos, rst_articles_neg, neg_upper_bound,
+                  ner_data_pos_fp, ner_data_neg_fp):
+    """
+    遍历文章
+    :param relation:
+    :param neg_upper_bound:
+    :param ner_data_pos_fp:
+    :param ner_data_neg_fp:
+    :return:
+    """
+    save_content(''.join(rst_articles_pos), ner_data_pos_fp)
+    save_content(''.join(rst_articles_neg[:neg_upper_bound]), ner_data_neg_fp)
+
+
+def gen_classify_data(rst_articles_pos, rst_articles_neg, neg_upper_bound,
+                      classify_data_pos_fp, classify_data_neg_fp):
+    rst_articles_pos = [r.replace('\n', '').split('\t') for r in
+                        rst_articles_pos]
+    rst_articles_neg = [r.replace('\n', '').split('\t') for r in
+                        rst_articles_neg]
+    with open(classify_data_pos_fp, 'a', encoding='utf-8') as fcp:
+        for rp in rst_articles_pos:
+            left = rp[2]
+            right = rp[3]
+            try:
+                left_events = get_events(left)
+                right_events = get_events(right)
+                line = rp + [left_events, right_events]
+                line = '\t'.join(line) + '\n'
+                fcp.write(line)
+            except Exception as e:
+                print(e)
+    with open(classify_data_neg_fp, 'a', encoding='utf-8') as fcn:
+        for i, rp in enumerate(rst_articles_neg):
+            if i > neg_upper_bound:
                 continue
-            lines = lines[1:]
-            content = ''.join(lines)
-            content = remove_white_space(content)
-            rsts, rsts_neg, neg_num = extract_articles(content, relation,
-                                                       neg_num, False)
-            rst_articles.extend(rsts)
-            rst_articles_neg.extend(rsts_neg)
-    save_content(''.join(rst_articles), ner_data_fn)
-    save_content(''.join(rst_articles_neg[:20000]), ner_data_neg_fn)
+            try:
+                events = get_events(rp[0])
+                line = rp + [events]
+                line = '\t'.join(line) + '\n'
+                fcn.write(line)
+            except Exception as e:
+                print(e)
 
 
-def add_pos_events(new_ner_data_fn):
-    with open(ner_data_fn, 'r', encoding='utf-8') as f:
+
+def gen_neg_by_other(relation):
+    contrast_neg_f = r'C:\work1\qingdao\dev\python\jdqd\a04\relation\relation_pt\services\contrast_neg.txt'
+    new_lines = []
+    with open(contrast_neg_f, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-        with open(new_ner_data_fn, 'a', encoding='utf-8') as f_w:
-            for line in lines:
-                splits = line.split('\t')
-                left = splits[2]
-                right = splits[3]
-                try:
-                    left_events = get_events(left)
-                    right_events = get_events(right)
-                    splits[4] = left_events
-                    splits[5] = right_events
-                    new_line = '\t'.join(splits) + '\n'
-                    f_w.write(new_line)
-                except Exception as e:
-                    print(e)
+        for line in lines:
+            splits = line.split('\t')
+            sentence = splits[0]
+            is_parallel = bool(
+                relation_combine.extract_all_rules(sentence, relation.rules,
+                                                   relation.keyword_rules))
+            if not is_parallel:
+                new_lines.append(line)
+    save_content(''.join(new_lines), 'data_parallel_e_neg.txt')
 
 
+def filter_events():
+    new_lines = []
+    with open(
+        r'C:\work1\qingdao\dev\python\jdqd\a04\relation\relation_pt\services\data_parallel_e.txt',
+        'r', encoding='utf-8') as f:
+        fs = list(set(f.readlines()))
+        for line in fs:
+            conts = line.strip().split('\t')
+            if len(conts) == 6 and '丨' not in line:
+                left_event = conts[4]
+                right_event = conts[5]
+                if len(left_event) > 3 and len(right_event) > 3:
+                    new_line = '\t'.join(
+                        [conts[0], left_event, right_event]) + '\n'
+                    new_lines.append(new_line)
+    save_content(''.join(new_lines), 'data_parallel_e_filter.txt')
 
-def add_neg_events(new_ner_data_neg_fn, neg_num, neg_capacity):
-    with open(ner_data_neg_fn, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
-        with open(new_ner_data_neg_fn, 'a', encoding='utf-8') as f_w:
-            for line in lines:
-                splits = line.split('\t')
-                left = splits[2]
-                right = splits[3]
-                try:
-                    left_events = get_events(left)
-                    right_events = get_events(right)
-                    splits[4] = left_events
-                    splits[5] = right_events
-                    new_line = '\t'.join(splits) + '\n'
-                    f_w.write(new_line)
-                except Exception as e:
-                    print(e)
 
 if __name__ == '__main__':
     ner_data_fn = 'data_parallel.txt'
     ner_data_neg_fn = 'data_parallel_neg.txt'
     new_ner_data_fn = 'data_parallel_e.txt'
-    add_pos_events(new_ner_data_fn)
+    # add_pos_events(new_ner_data_fn)
+    # gen_neg_by_other(r_parallel)
+    filter_events()
     pass
 
     # for id_, content in articles_zh[:5]:
